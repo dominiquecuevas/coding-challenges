0ee125b8140931914744429562b2e371f7898af6
Dominique Cuevas <cuevasdominique@gmail.com>
2020-02-20 17:09:28 -0800

    leetcode contains duplicates

---
+++ contains-duplicates.py
@@ -0,0 +1,19 @@
+def contains_duplicates(lst):
+    """
+    Runtime is O(n log n). Sorting is O(n log n) and the loop is O(n) 
+    >>> contains_duplicates([1,2,3,1])
+    True
+    >>> contains_duplicates([1,2,3,4])
+    False
+    """
+    lst.sort()
+    for idx in range(1, len(lst)):
+        if lst[idx-1] == lst[idx]:
+            return True
+    return False
+
+if __name__ == "__main__":
+    import doctest
+    result = doctest.testmod()
+    if result.failed == 0:
+        print("All tests passed.")
