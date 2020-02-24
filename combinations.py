82f7457424fef411872886f89dd0263db1542511
Dominique Cuevas <cuevasdominique@gmail.com>
2020-02-24 00:36:25 -0800

    edge case and set largest num

--- combinations.py
+++ combinations.py
@@ -3,18 +3,13 @@ def combinations(n, k):
     Leetcode 77. Given two integers n and k, 
     return all possible combinations of k numbers out of 1 ... n.
     """
-    # create a set of nums
-    # turn set into list
-    # have pointer to 0 index, then loop through list
-    # increment pointer
-    if n >= k:
-        largest = n
-    else:
-        largest = k
+    largest = n if n >= k else k
+    if largest == 1:
+        return [[1]]
     nums = range(1, largest+1)
     pointer = 0
     result = []
-    while pointer < len(nums) - 1:
+    while pointer <= len(nums) - 1:
         for i in range(pointer+1, len(nums)):
             result.append([nums[pointer], nums[i]])
         pointer +=1
