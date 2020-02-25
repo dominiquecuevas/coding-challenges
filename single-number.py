9cd19a37f12c427dae1477bc1c989ba5e259b600
Dominique Cuevas <cuevasdominique@gmail.com>
2020-02-24 21:58:39 -0800

    leetcode single_number_ solved with dictionary

--- single-number.py
+++ single-number.py
@@ -1,4 +1,10 @@
 def single_number(nums):
+    """
+    >>> single_number([2,2,1])
+    1
+    >>> single_number([4,1,2,1,2])
+    4
+    """
     
     nums.sort()
     i = 0
@@ -11,5 +17,23 @@ def single_number(nums):
             i += 2
             return nums[i]
 
-print(single_number([2,2,1]))
-print(single_number([4,1,2,1,2]))
+def single_number_(nums):
+    """
+    >>> single_number_([2,2,1])
+    1
+    >>> single_number_([4,1,2,1,2])
+    4
+    """
+    dict_nums = {}
+    for i in nums:
+        try:
+            dict_nums.pop(i)
+        except:
+            dict_nums[i] = 1
+    return dict_nums.popitem()[0]
+
+if __name__ == "__main__":
+    import doctest
+    result = doctest.testmod()
+    if result.failed == 0:
+        print("All tests passed!")
